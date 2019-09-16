d44e5cac31a17f7d32fd31f49d85b82367761a43
rimhoho <rimhoho@gmail.com>
2019-09-16 16:44:36 -0400

    Replace Old folder to New one

---
+++ Hwang_R_documents/Hwang_Introduction.R
@@ -0,0 +1,17 @@
+2+3
+x <- 4
+x+2
+my_num <- 2/2
+my_chr <- "hi!"
+my_bool <- TRUE
+typeof(my_num)
+
+typeof(my_chr)
+typeof(my_bool)
+
+my_nan <- 0/0
+my_nan
+
+install.packages("tidyverse")
+my_nan <- 0/0
+help(type)
